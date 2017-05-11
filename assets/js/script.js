36d8c1f4063c2aa9518ac06d7519fe4c09dd964f
Jirattikarn <jirattikarn@Wonderbright-2.local>
2017-05-11 16:07:03 +0700

    Add files

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $('.knoware-mobile-close, .knoware-mobile-menu').click(function(){
+      $('.knoware-overlay').toggleClass('knoware-overlay-active');
+    });
+});
